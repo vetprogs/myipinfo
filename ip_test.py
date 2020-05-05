e260d4b446cb82db65561d983a8c98e843abfc68
vetprogs <64850135+vetprogs@users.noreply.github.com>
2020-05-05 15:00:45 +0000

    Add files via upload

---
+++ ip_test.py
@@ -0,0 +1,16 @@
+#Автор https://github.com/vetprogs
+#По вопросам и предложениям:  vetprogs(сoбakа)etlgr.com
+
+import time
+import requests
+
+st_run=time.time()
+myip=requests.get('https://ifconfig.me').text.strip() #узнать внешний IP
+apiget = 'https://www.iplocate.io/api/lookup/'+myip
+zapros=requests.get(apiget)   #получаем информацию про IP
+
+for k, v in zapros.json().items(): 
+    if v:                     #показываем только не пустые ключи  
+        print(f"{k}:{v}")
+
+print("Выполнено за: {:1.2f} ms".format((time.time()-st_run)*1000))
